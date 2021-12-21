1209505c55540c98162fd1cef6099cc9dc5ea9bb
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2021-12-21 20:26:20 +0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -14,19 +14,19 @@
        science =+ sub4.value;
      let  obtainedMarks = eng + urdu + math + science;
      let  percentage = obtainedMarks/totalNumber * 100;
-     if (percentage >= 90 || percentage <= 100 ) {
+     if (percentage >= 90 && percentage <= 100 ) {
          status1.innerText = 'your grade is A';
-      } else if (percentage >= 80 || percentage <= 89 ) {
+      } else if (percentage >= 80 && percentage <= 89 ) {
           status1.innerText = 'your grade is B';
-      }else if (percentage >= 70 || percentage <= 79 ){
+      }else if (percentage >= 70 && percentage <= 79 ){
           status1.innerText = 'your grade is C'
-      }else if (percentage >= 60 || percentage <= 69) {
+      }else if (percentage >= 60 && percentage <= 69) {
         status1.innerText = 'your grade is D'
-    }else if (percentage >= 50 || percentage <= 59) {
+    }else if (percentage >= 50 && percentage <= 59) {
         status1.innerText = 'your grade is E'
-    }else if (percentage >= 40 || percentage <= 49) {
+    }else if (percentage >= 40 && percentage <= 49) {
         status1.innerText = 'your grade is F'
-    }else if (percentage >= 33 || percentage <= 39) {
+    }else if (percentage >= 33 && percentage <= 39) {
         status1.innerText = 'your grade is lower F'
     }else if (percentage < 33) {
         status1.innerText = ' FAIL ! try again next time'
